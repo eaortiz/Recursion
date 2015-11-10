980b651dab62827f89d2915461a1438bd0f649c2
Estefania Ortiz <eaortiz@stanford.edu>
2015-11-10 12:53:02 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,19 @@ import com.sun.deploy.util.StringUtils;
 public class Main {
 
     public static void main(String[] args) {
-
+        //more test cases would be good
         System.out.println(isEvenWord("tattletale"));
         System.out.println(isEvenWord("arraigning"));
         System.out.println(isEvenWord("banana"));
 
     }
 
-
-
-
-    private static boolean isEvenWord(String s)
+    private static boolean isEvenWord(String s) // s could be a better variable name
     {
         if (s.length() == 0)
             return true;
 
-        String search = String.valueOf(s.charAt(0));
+        String search = String.valueOf(s.charAt(0)); // again search could be named better
         s = s.substring(1);
 
         if(s.contains(search)) {
@@ -33,4 +30,5 @@ public class Main {
 
     }
 
+    //clean and concise, I like it!
 }
